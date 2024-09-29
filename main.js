e8dbf3a43f61a70bdf83f9acfc134102234195f1
Riman Das <34216663+rimanz@users.noreply.github.com>
2024-09-29 11:34:02 +0600

    Add functionality to get user input

--- main.js
+++ main.js
@@ -4,8 +4,22 @@ function getComputerChoice() {
     return choices[index];
 }
 
+function getHumanChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let choice = prompt(`
+        Please make choice:
+            1. Rock
+            2. Paper
+            3. Scissors
+
+        Please enter the respective number of your choice (without the ".")!
+        `);
+    let index = choice - 1;
+    return choices[index];
+}
+
 
 
 
 // Testing:
-console.log(getComputerChoice());
+console.log(getHumanChoice());
